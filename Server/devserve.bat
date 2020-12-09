92de14eea8088bc371b90f99d08a6018f77fcc17
SimonFJ20 <simonfromjakobsen@gmail.com>
2020-12-09 16:40:05 +0100

    Moving static files

--- Server/devserve.bat
+++ Server/devserve.bat
@@ -1,5 +1,12 @@
 @echo off
+
+set FLASK_APP=server.py
+set FLASK_ENV=dev
+
 :loop
+
 python -m flask run
+
 pause
+
 goto loop
--- Server/server.py
+++ Server/server.py
@@ -5,22 +5,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    url_for('static', filename='inherit.css')
     return render_template('index.html')
 
 @app.route('/uploadrequest')
 def uploadrequest():
     return render_template('uploadrequest.html')
-    
+
 @app.route('/uploadrequest/datahandler', methods=['POST', 'GET'])
 def uploadrequestdatahandler():
     error = None
     if request.method == 'POST':
         return "bruh"
     return render_template('datahandler.html')
-    
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 80))
     app.run(host='0.0.0.0', port=port)
-
-    
