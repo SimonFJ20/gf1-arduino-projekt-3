6702cf90e3c907452b2e2bdfabb9f025ae155329
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-04-24 20:19:19 +0200

    more api ts

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,14 @@
 import { Router } from 'express';
-import mongodb from 'mongodb';
+import mongodb, { ObjectId } from 'mongodb';
 import dotenv from 'dotenv';
+
+interface SketchInterface {
+    title: string,
+    description: string,
+    author: string,
+    content: string,
+    _id?: ObjectId
+}
 
 const router = Router();
 
@@ -8,7 +16,6 @@ const api = async () => {
 
     dotenv.config()
     const mongoURI = process.env.MONGODB;
-    console.log(mongoURI)
     const client = new mongodb.MongoClient(mongoURI!, {useNewUrlParser: true, useUnifiedTopology: true});
 
     try {
@@ -27,12 +34,27 @@ const api = async () => {
 
     router.post('/sketches/create', async (req, res) => {
         try {
-            const sketch = {
+            
+            if(!req.body.title || !req.body.description || !req.body.author || !req.body.content) {
+                res.status(400).json({success: false, status: 'incomplete'});
+                return;
+            }
+
+            const sketch: SketchInterface = {
                 title: req.body.title,
                 description: req.body.description,
                 author: req.body.author,
                 content: req.body.content
             }
+
+            const existingTitle: SketchInterface = await Sketches.findOne({title: sketch.title});
+            if(existingTitle) {
+                res.status(400).json({success: false, status: 'title taken'});
+                return;
+            }
+
+            const insert = await Sketches.insertOne(sketch);            
+            res.json({success: true, status: 'success', insert: insert.ops[0]})
         } catch(error) {
             res.status(500).json({success: false, status: 'error'});
             console.error('Error on /sketches/create', error)
