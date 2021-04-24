8419c4777287ae3a3ef73bb347bc1f357460f47c
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-04-24 03:20:06 +0200

    added https

---
+++ src/api.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+export const apiRoutes = (): Router => {
+    const router = Router();
+
+
+
+
+
+    return router;
+}
+
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import http from 'http';
+import https from 'https';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import path from 'path';
+import { apiRoutes } from './api';
+
 
 dotenv.config();
 const server = express();
@@ -10,10 +14,25 @@ server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({extended: true}));
 
+server.use('/api', apiRoutes);
 server.use(express.static(path.join(__dirname, '../public')));
 
-const port = parseInt(process.env.HTTP_PORT!);
-server.listen(port, () => {
-    console.log('Server run on port', port);
-})
+try {
+    const httpPort = parseInt(process.env.HTTP_PORT!);
+    const httpsPort = parseInt(process.env.HTTPS_PORT!);
 
+    
+    server.listen()
+
+    http.createServer(server).listen(httpPort, () => {
+        console.log('HTTPS on port', httpPort);
+    })
+
+    https.createServer({}, server).listen(httpsPort, () => {
+        console.log('HTTPS on port', httpsPort);
+    })
+
+    
+} catch(error) {
+    console.error(error);
+}
