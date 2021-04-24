ee000d6e898b36d70c87ec0bfcdee4722863a8e6
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-04-24 04:12:09 +0200

    api

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,45 @@
 import { Router } from 'express';
+import mongodb from 'mongodb';
+import dotenv from 'dotenv';
 
-export const apiRoutes = (): Router => {
-    const router = Router();
+const router = Router();
 
+const api = async () => {
 
+    dotenv.config()
+    const mongoURI = process.env.MONGODB;
+    console.log(mongoURI)
+    const client = new mongodb.MongoClient(mongoURI!, {useNewUrlParser: true, useUnifiedTopology: true});
 
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB Cloud');
+    } catch(e) {
+        console.error(e);
+    }
+    
+    const database = client.db('ArduinoProjekt3');
+    const Sketches = database.collection('sketches');
 
+    router.get('/sketches/getall', async (req, res) => {
+        
+    });
 
-    return router;
+    router.post('/sketches/create', async (req, res) => {
+        try {
+            const sketch = {
+                title: req.body.title,
+                description: req.body.description,
+                author: req.body.author,
+                content: req.body.content
+            }
+        } catch(error) {
+            res.status(500).json({success: false, status: 'error'});
+            console.error('Error on /sketches/create', error)
+        }
+    })
+
 }
 
+api();
+export default router;
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import https from 'https';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import path from 'path';
-import { apiRoutes } from './api';
+import fs from 'fs';
+import api from './api';
 
 
 dotenv.config();
@@ -14,21 +15,22 @@ server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({extended: true}));
 
-server.use('/api', apiRoutes);
+server.use('/api', api);
 server.use(express.static(path.join(__dirname, '../public')));
+
 
 try {
     const httpPort = parseInt(process.env.HTTP_PORT!);
     const httpsPort = parseInt(process.env.HTTPS_PORT!);
 
-    
-    server.listen()
+    const sslCert = fs.readFileSync(path.join(__dirname, '../ssl/cert.crt'));
+    const sslKey = fs.readFileSync(path.join(__dirname, '../ssl/key.key'));
 
     http.createServer(server).listen(httpPort, () => {
-        console.log('HTTPS on port', httpPort);
+        console.log('HTTP on port', httpPort);
     })
 
-    https.createServer({}, server).listen(httpsPort, () => {
+    https.createServer({cert: sslCert, key: sslKey}, server).listen(httpsPort, () => {
         console.log('HTTPS on port', httpsPort);
     })
 
