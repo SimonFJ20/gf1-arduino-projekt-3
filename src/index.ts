917b1f6a020dfdcfd107ca5fa1140478fdcd19a7
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-04-24 02:17:51 +0200

    public working

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,11 @@ import path from 'path';
 dotenv.config();
 const server = express();
 
-server.use(cors);
-server.use(express.urlencoded({extended: true}));
+server.use(cors());
 server.use(express.json());
+server.use(express.urlencoded({extended: true}));
 
-
-server.use('/', express.static(path.join(__dirname, 'public')));
+server.use(express.static(path.join(__dirname, '../public')));
 
 const port = parseInt(process.env.HTTP_PORT!);
 server.listen(port, () => {
